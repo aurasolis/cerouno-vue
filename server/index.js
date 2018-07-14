9ccfe5e5fd9e55778e59e3b2c7258ff4cc0932a3
aurasolis <sol@reservamos.mx>
2018-07-14 11:14:50 -0500

    Modified index.js

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-var {Song} = require('./db')
+var { Song } = require('./db')
 var restify = require('restify')
 var server = restify.createServer()
 var port = 3001
@@ -22,6 +22,16 @@ server.get('/songs', (request, response) => {
 	response.end()
   })
 })
+//curl localhost:3001/songs
+
+server.post('/songs', (request, response) => {
+  const body = JSON.parse(request.body)
+  Song.create(body).then(song => {
+    response.send(body)
+    response.end()
+  })
+})
+//curl -X POST -d '{"name": "La Macarena"}' localhost:3001/songs
 
 server.listen(port, () => {
   console.log(`Verizon stub server running on port ${port}`)
